041f94fb7f4a0f8ca80f17febc3532d6e2f13ea2
Jason <lavidaguitarist.composer@gmail.com>
2025-03-29 19:12:44 -0700

    First commit

---
+++ start.c
@@ -0,0 +1,8 @@
+#include <stio.h>
+
+int main(){
+    printf("This is something that I'm writing in vscode:w
+    ");
+    printf("This is something to say and write right now in Visual Studio Code ");
+    return 0;
+}
