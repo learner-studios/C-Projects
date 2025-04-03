b50144fc870000815b68a33b7426af1452cb06b1
Jason <jasonlearnerstudios@gmail.com>
2025-04-02 20:56:57 -0700

    working on the array_study homework section

---
+++ array_study.c
@@ -0,0 +1,22 @@
+//notes for arrays 
+#include <stdio.h>
+//#define my_first_array = 32; 
+
+
+int main (){
+
+    //int my_first_array[50];
+
+    //set the 5th element in the array with a value
+
+    my_first_array[5] = 10;
+
+    //now to the 49th element
+
+    my_first_array[49] = 1;
+
+    //print what's in the arrays
+
+    printf("%d\n", my_first_array[5]);
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world!\n");
+    printf("This is another program that I'm writing in VSCode\n");
+    return 0;
+}
